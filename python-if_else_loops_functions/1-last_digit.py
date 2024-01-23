a75f390a9b00ecc8c7f8db393a81dce1f5b467ef
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-23 13:20:07 -0800

    last digit return true result for negative value

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = number % 10
+digit = abs(number) % 10
+if number < 0:
+    digit *= -1
+
 if digit > 5:
     print(f"Last digit of {number} is {digit} and  is greater than 5")
 elif digit == 0:
