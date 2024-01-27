65fae5dce1cd61c4cd07dfba1e46708a109d1652
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-27 12:38:34 -0800

    change print format

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 
 if __name__ == "__main__":
     import sys
-count = len(sys.argv) - 1
+
 total = 0
-for i in range(count):
+for i in range(len(sys.argv) - 1):
     total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    print(total)
