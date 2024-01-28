cbc9e9fd9cc24c1915b707633e9e10234fc36bdf
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-28 10:23:43 -0800

    multiples returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        sentence[0] == None
+    length = len(sentence)
+    first_elem = sentence[0]
+    return (length, first_elem)
+
