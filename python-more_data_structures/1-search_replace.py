6806f21b23cee95a78cb31c9b73c191126d6a299
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-29 05:54:54 -0800

    replace elemt

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,5 +2,7 @@
 def search_replace(my_list, search, replace):
     if len(my_list) >= 0:
         new_list = my_list[:]
-        new_list[search] = replace
+        for i in range(len(new_list)):
+            if new_list[i] == search:
+                new_list[i] = replace
     return new_list
