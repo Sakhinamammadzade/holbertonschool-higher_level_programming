40d25d1c3299a426a15e3d8421a2560d0c3cf451
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-30 13:57:21 -0800

    total of uniq num

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    total = 0
+    new_list = set()
+    if len(my_list) > 0:
+        for num in my_list:
+            if num not in new_list:
+                new_list.add(num)
+                total += num
+        return total
