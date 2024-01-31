14a5b674125d0b75799334dcdf78411df1dc6c1f
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-31 02:42:09 -0800

    modified multiply

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     for key, value in a_dictionary.items():
-        print(f"{key}: {value}", value*2)
+        new_dict[key] = value*2
+    return new_dict
