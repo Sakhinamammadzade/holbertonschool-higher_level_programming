2d3775ac53a59e250bd6189f84e0ccb361b1c843
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-01 13:39:52 -0800

    correct output fixed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,11 +4,12 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end='')
+            print(my_list[i], end='')
             count += 1
 
-        print()
+    
     except TypeError:
         pass
     finally:
+        print()
         return count
