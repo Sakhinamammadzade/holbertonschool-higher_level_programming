f9dd64b4d91814416143492c53c28816d75b46a2
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-01 13:42:11 -0800

    pycodestyle fixed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,8 +6,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             count += 1
-
-    
     except TypeError:
         pass
     finally:
