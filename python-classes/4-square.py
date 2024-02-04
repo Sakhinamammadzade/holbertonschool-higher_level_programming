a5b29c990ea9d313d742674e74e1770f056143c9
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-04 01:56:40 -0800

    class is documented

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Access and update private attribute."""
+"""Define a Square class."""
 
 
 class Square:
@@ -9,7 +9,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
