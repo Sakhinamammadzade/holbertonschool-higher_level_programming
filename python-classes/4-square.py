0ec77bcfe4e427e1d46fda9ee637389c294a1359
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-04 01:49:20 -0800

    Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Access and update private attribute."""
+
+
+class Square:
+    def __init__(self, size=0):
+        """Function for access and update attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+
+    @property
+    def size(self):
+        """property to retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter to set new size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Function for area of size"""
+        return self.__size ** 2
