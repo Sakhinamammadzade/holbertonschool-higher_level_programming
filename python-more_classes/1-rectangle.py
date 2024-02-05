2901ce9066329c47c7b420e5321e57407bcb01ef
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-05 12:57:34 -0800

    hot fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     "Define a Rectangle class"
     def __init__(self, width=0, height=0):
         """Function for access and update attribute"""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
