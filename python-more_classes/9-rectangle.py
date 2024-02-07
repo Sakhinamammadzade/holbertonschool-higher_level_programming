221ee994666bbd127507cf5f6965dc52b5f62968
Sakina Mammadzade <96643702+Sakhinamammadzade@users.noreply.github.com>
2024-02-07 21:59:26 +0400

    Update 9-rectangle.py

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -89,5 +89,5 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         return cls(size, size)
