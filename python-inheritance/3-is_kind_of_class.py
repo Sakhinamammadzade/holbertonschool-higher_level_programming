3c0b6ba237c7dba1d742d316b815ae5c99356948
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-10 15:58:09 -0800

    is kind of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """object is an instance of, or if the object is an instance of a class"""
+    return isinstance(obj, a_class)
