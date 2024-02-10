b2b9cbd06566b54ace9f6aeda36bc46d9333885d
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-10 15:03:19 -0800

    sorting list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" My list"""
+
+
+class MyList(list):
+    """Define my list sorting ascending """
+    def print_sorted(self):
+        """Function for sorting list"""
+        return sorted(self)
