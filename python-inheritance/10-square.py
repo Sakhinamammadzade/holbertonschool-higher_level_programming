9715e6de2a67bc4f89eb0aebca41df15496fffb2
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-11 08:34:54 -0800

    PEP8 validation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,6 +4,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Define new class"""
     def __init__(self, size):
