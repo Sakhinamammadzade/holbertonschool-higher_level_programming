ea972a71ac42664712e92bea0bfe2e33089a6b21
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-11 08:31:59 -0800

    super init

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Define new class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
