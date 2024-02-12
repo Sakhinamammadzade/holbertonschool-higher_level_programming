d046840290f114442154921e09788b0ef2cde34b
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-12 05:44:12 -0800

    pycodestyle fixed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is  None:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
