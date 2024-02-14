8d491fe732ca7f13fe597983106848aa57ccb1e5
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-14 02:19:49 -0800

    display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,8 +75,10 @@ class Rectangle(Base):
 
     def display(self):
         """print the rectangle with the character #"""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Str"""
