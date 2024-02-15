94dcf8826a2907aa0f79e8fa00953c091943e85d
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-14 17:37:10 -0800

    Square instance to dictionary representation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -42,3 +42,9 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """rectangle instance to dictionary"""
+        return {
+                "id": self.id,
+                "x": self.x,
+                "size": self.size,
+                "y": self.y
+                }
