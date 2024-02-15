c9628bf3f3fde70ae8e6acccda4ee5be45440a91
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-15 04:44:46 -0800

    json to dict

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base Class"""
-
+import json
 
 class Base():
     """Define Base"""
@@ -12,3 +12,12 @@ class Base():
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Dictionary to json string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            with open("Rectangle.json", "w") as outfile:
+                return json.dumps(list_dictionaries)
