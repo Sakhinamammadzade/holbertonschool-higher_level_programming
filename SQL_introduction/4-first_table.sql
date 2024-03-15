fbf819858a1379b9eed04cfa36406308ead303e2
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-15 18:23:10 +0400

    full description table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
--- creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS `first_table`(
-	`id` int,
-	`name` varchar(256)
-);
+-- creates a table called first_table in the current database in your MySQL server.i
+CREATE TABLE IF NOT EXISTS `first_table` (`id` int , `name` varchar(256));
