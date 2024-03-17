f2d599a9227107f45232694321be500e318ddf1e
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-18 01:19:50 +0400

    add attribute

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -2,8 +2,8 @@
 """add_attribute"""
 
 
-def add_attribute(obj, attribute, value):
+def add_attribute(obj, value):
     """Add attribute to the object if possible, otherwise raise TypeError."""
     if not hasattr(obj, '__dict__') and not hasattr(obj, '__slots__'):
         raise TypeError("can't add new attribute")
-    setattr(obj, attribute, value)
+    setattr(obj, value)
