099fc8ae012206b14d5e0db0f0df0ea8cf9f8485
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-18 01:35:33 +0400

    Add a new attribute to an object if possible.

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -4,11 +4,6 @@
 
 def add_attribute(obj, attribute, value):
     """Add attribute to the object if possible, otherwise raise TypeError."""
-    if not hasattr(obj, '__dict__') and not hasattr(obj, '__slots__'):
-        raise TypeError("can't add new attribute")
-    if not isinstance(attribute, str):
-        raise TypeError("attribute name must be a string")
-    if hasattr(obj, attribute):
-        raise TypeError("attribute already exists")
-    
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute") 
     setattr(obj, attribute, value)
