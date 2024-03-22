5f229fb09593479c03be416cead90a122394666e
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-23 00:40:55 +0400

    fixxx

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,6 @@
 -- create db and table
 CREATE database IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.states`
+(id INT UNIQUE NOT NULL AUTO_INCREMENT, 
+name VARCHAR(256), 
+PRIMARY KEY(id));
