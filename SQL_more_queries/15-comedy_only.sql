0b54d0ec9972d485e73caf8c67b1510793fecea8
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-24 02:02:16 +0400

    idk

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE name = 'Comedy'
-ORDER BY tv_genres.title;
+ORDER BY tv_shows.title;
