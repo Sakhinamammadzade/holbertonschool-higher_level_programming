bacefa45172db0b767cef49748e55e7406fc4a47
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-24 15:18:06 +0400

    hot fix

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,7 @@
 --  lists all genres in the database hbtn_0d_tvshows_rate by their rating
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
 ORDER BY rating DESC;
