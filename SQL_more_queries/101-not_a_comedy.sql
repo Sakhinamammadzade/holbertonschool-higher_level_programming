07299ea27f50921c477ca4ab3cccaee66b80c4ce
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-24 14:16:20 +0400

    No Comedy tonight!

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,8 @@
 -- lists all shows without the genre Comedy in the database
-SELECT tv_showa.title
+SELECT tv_shows.title
 FROM tv_shows
 WHERE id NOT IN (
-	    SELECT tv_genres.id
+	    SELECT 1
 	    FROM tv_genres
 	    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 	    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
