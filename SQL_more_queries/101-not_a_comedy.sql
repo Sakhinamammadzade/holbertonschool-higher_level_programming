3614f29e681bc645e87a8ef8c660634b2ce957da
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-24 14:18:18 +0400

    No Comedy tonight!

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -3,9 +3,8 @@ SELECT tv_shows.title
 FROM tv_shows
 WHERE id NOT IN (
 	    SELECT 1
-	    FROM tv_genres
-	    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-	    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	    WHERE tv_genres.name = 'Comedy'
+	    FROM tv_show_genres
+	    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+	    WHERE tv_genres.name = 'Comedy' AND tv_show_genres.show_id = tv_shows.id
 )
 ORDER BY tv_shows.title;
