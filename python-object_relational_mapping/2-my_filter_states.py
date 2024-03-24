39ed2651de6afd8702f3a06bbc685ad70bd4bcf2
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-03-24 16:48:00 +0400

    filter state by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,9 +13,10 @@ if __name__ == "__main__":
         host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 
     cursor = db_connection.cursor()
-    query = ("""SELECT * FROM states WHERE name LIKE
+    query = """SELECT * FROM states WHERE name LIKE
                    BINARY '{}' ORDER BY id""".format(argv[4])
     cursor.execute(query)
+
     rows = cursor.fetchall()
 
     for row in rows:
