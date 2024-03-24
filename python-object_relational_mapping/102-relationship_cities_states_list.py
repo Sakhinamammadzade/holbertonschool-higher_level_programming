0cca2e43a311a0d8cfc02660a7d8ab839acbbb34
Sakina Mammadzade <96643702+Sakhinamammadzade@users.noreply.github.com>
2024-03-24 23:56:39 +0400

    102

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -4,34 +4,29 @@
     “San Francisco” from the database hbtn_0e_100_usa
 """
 
+import sys
 from relationship_state import Base, State
 from relationship_city import City
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
 
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(engine)
 
-    new_state = State(name="California")
-    new_city = City(name="San Francisco", state=new_state)
-
-    session.add(new_city)
-    session.commit()
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
 
     session.close()
